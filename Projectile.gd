c691bdee2d8df3cb3b17801c722c6784a7711462
Skorpio <skorpio2011@yahoo.de>
2018-02-24 02:45:09 +0100

    Players don't collide with their own projectiles anymore.

--- Projectile.gd
+++ Projectile.gd
@@ -10,16 +10,18 @@ func _ready():
 
 
 func init(player):
-	var vec = Vector2(500, 0).rotated(player.rotation)
+	var vec = Vector2(583, 0).rotated(player.rotation)
 	linear_velocity = vec
-	position = player.position + vec / 6
+	position = player.position + vec / 9
 	rotation = player.rotation
 	var color = player.get_node('Polygon2D').color
 	$Polygon2D.color = color
 	self.color = color
+	add_collision_exception_with(player)
 
 
 func _process(delta):
+	# Perhaps it would be better to use a signal.
 	if get_colliding_bodies():
 		var particles = collision_particles.instance()
 		particles.emitting = true
